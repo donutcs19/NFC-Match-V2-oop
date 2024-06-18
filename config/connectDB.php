57f1d4189ab43bfc9f592cac21fc26c3c741b572
ShiKikie <61870426+donutcs19@users.noreply.github.com>
2024-06-18 12:32:13 +0700

    Add files via upload

---
+++ config/connectDB.php
@@ -0,0 +1,24 @@
+<?php 
+class Database {
+    private $host = "localhost";
+    private $db = "card_rfid";
+    private $username ="";
+    private $password ="";
+
+    private $charset = "utf8";
+    public $conn;
+
+    public function getConnection(){
+        $this->conn = null;
+
+        try{
+            $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->db.";charset=".$this->charset.";", $this->username, $this->password);
+            // echo "[Success] -> Connect successfuly ";
+        }catch(PDOException $e){
+            echo "[Error] -> ".$e->getMessage();
+        }
+        return $this->conn;
+    }
+
+}
+?>
